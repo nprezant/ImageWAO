3a5a53ce26b6c9f1e505ef1c0822462a2d3408c1
nprezant <34352369+nprezant@users.noreply.github.com>
2020-02-16 15:58:32 -0500

    Allow docked widgets to go on both the left and right side of the screen

--- src/main/python/imagewao.py
+++ src/main/python/imagewao.py
@@ -51,28 +51,28 @@ class QImageWAO(QtWidgets.QMainWindow):
     def _setGrid(self, grid):
         self.grid = grid
         self.gridDock = QtWidgets.QDockWidget('Image Grids', self)
-        self.gridDock.setAllowedAreas(QtCore.Qt.RightDockWidgetArea)
+        self.gridDock.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea)
         self.gridDock.setWidget(self.grid)
         self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.gridDock)
 
     def _setLibrary(self, library):
         self.library = library
         self.libraryDock = QtWidgets.QDockWidget('Library', self)
-        self.libraryDock.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea)
+        self.libraryDock.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea)
         self.libraryDock.setWidget(self.library)
         self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self.libraryDock)
 
     def _setAnimalAdder(self, animal_adder):
         self.animalAdder = animal_adder
         self.animalAdderDock = QtWidgets.QDockWidget('Add New Animal', self)
-        self.animalAdderDock.setAllowedAreas(QtCore.Qt.RightDockWidgetArea)
+        self.animalAdderDock.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea)
         self.animalAdderDock.setWidget(self.animalAdder)
         self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.animalAdderDock)
 
     def _setAnimalTotals(self, animal_totals):
         self.animalTotals = animal_totals
         self.animalTotalsDock = QtWidgets.QDockWidget('Total Animal Counts', self)
-        self.animalTotalsDock.setAllowedAreas(QtCore.Qt.RightDockWidgetArea)
+        self.animalTotalsDock.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea)
         self.animalTotalsDock.setWidget(self.animalTotals)
         self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.animalTotalsDock)
 
