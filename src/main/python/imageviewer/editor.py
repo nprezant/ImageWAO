4a2dfe17d93ce637657dcccba3c7c22b64368328
nprezant <34352369+nprezant@users.noreply.github.com>
2020-02-27 04:30:00 -0500

    Baseline code for multiple color changing drawing icons

--- src/main/python/imageviewer/editor.py
+++ src/main/python/imageviewer/editor.py
@@ -28,15 +28,34 @@ class QImageEditor(QImageViewer):
         # Color palette popup menu
         self._colorMenu = ColorMenu(config.colors)
         self._colorMenu.colorChanged.connect(self._penColorChanged)   
-        self._colorMenu.reset() # Nesessary to catch the signal for the default color
 
         # Assign menu to button & button to toolbar
         self.colorButton.setMenu(self._colorMenu)
+
+        # Drawing buttons
+        self.ovalButton = QtWidgets.QToolButton(self)
+        self.rectButton = QtWidgets.QToolButton(self)
+        self.lineButton = QtWidgets.QToolButton(self)
+
+        # Drawing icn masks
+        self.ovalMask = QtGui.QPixmap(ctx.get_resource('icons/ic_oval.png'))
+        self.rectMask = QtGui.QPixmap(ctx.get_resource('icons/ic_rect.png'))
+        self.lineMask = QtGui.QPixmap(ctx.get_resource('icons/ic_line.png'))
+
+        # Add buttons to toolbar
         self.toolbar.addWidget(self.colorButton)
+        self.toolbar.addWidget(self.ovalButton)
+        self.toolbar.addWidget(self.rectButton)
+        self.toolbar.addWidget(self.lineButton)
+        
+        self._colorMenu.reset() # Nesessary to catch the signal for the default color
 
     @QtCore.Slot(QtGui.QColor)
     def _penColorChanged(self, qcolor):
         self.colorButton.setIcon(ColorMenu.maskedIcon(qcolor, self.paletteMask))
+        self.ovalButton.setIcon(ColorMenu.maskedIcon(qcolor, self.ovalMask))
+        self.rectButton.setIcon(ColorMenu.maskedIcon(qcolor, self.rectMask))
+        self.lineButton.setIcon(ColorMenu.maskedIcon(qcolor, self.lineMask))
         print(f'Color changed to {qcolor}')
         
 
