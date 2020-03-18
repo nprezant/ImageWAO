1bb98a83591118176f0eadefce45e87cdc9f9bd5
nprezant <34352369+nprezant@users.noreply.github.com>
2020-03-17 20:00:16 -0400

    Add actual count form layout content

--- src/main/python/ui/counts.py
+++ src/main/python/ui/counts.py
@@ -10,11 +10,38 @@ class CountForm(PopupFrame):
 
     def __init__(self, parent):
         super().__init__(parent)
-        self.resize(100,100)
-        self.setStyleSheet('''
-            CountForm {
-                border: 1px solid black;
-                border-radius: 4px; 
-                background: red;
-            }
-        ''')
+
+        # Form labels and controls
+        animalLabel = QtWidgets.QLabel('Species', self)
+        animalLabel.setAlignment(QtCore.Qt.AlignCenter)
+        animalText = QtWidgets.QLineEdit(self)
+        
+        countLabel = QtWidgets.QLabel('Number', self)
+        countLabel.setAlignment(QtCore.Qt.AlignCenter)
+        countText = QtWidgets.QLineEdit(self)
+
+        duplicateLabel = QtWidgets.QLabel('Already Counted', self)
+        duplicateBox = QtWidgets.QCheckBox(self)
+
+        notesLabel = QtWidgets.QLabel('Notes', self)
+        notesText = QtWidgets.QPlainTextEdit(self)
+        notesText.viewport().setAutoFillBackground(False)
+
+        # Initialize form
+        layout = QtWidgets.QGridLayout(self)
+
+        # Prevents copy/paste/etc form from showing up
+        self.setContextMenuPolicy(QtCore.Qt.PreventContextMenu)
+
+        # Layout form items
+        layout.addWidget(animalLabel, 0, 0)
+        layout.addWidget(animalText, 0, 1)
+        layout.addWidget(countLabel, 1, 0)
+        layout.addWidget(countText, 1, 1)
+        layout.addWidget(duplicateLabel, 2, 1)
+        layout.addWidget(duplicateBox, 2, 0, alignment=QtCore.Qt.AlignHCenter)
+        layout.addWidget(notesLabel, 3, 0, 1, 2)
+        layout.addWidget(notesText, 4, 0, 1, 2)
+
+        self.setLayout(layout)
+        self.adjustSize()
