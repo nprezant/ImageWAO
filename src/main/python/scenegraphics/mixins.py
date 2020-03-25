53e1f4b210f97dd6cfa05d7044cb7e6885a007dc
nprezant <34352369+nprezant@users.noreply.github.com>
2020-03-25 11:10:11 -0400

    Update scene items to have a hover-effect

--- src/main/python/scenegraphics/mixins.py
+++ src/main/python/scenegraphics/mixins.py
@@ -17,6 +17,14 @@ class SceneCountsItemMixin:
     This is not a full class.
     '''
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Initialize drop shadow effect. Default to off.
+        self._shadowEffect = QtWidgets.QGraphicsDropShadowEffect()
+        self.setGraphicsEffect(self._shadowEffect)
+        self._shadowEffect.setEnabled(False)
+
     def setCountData(self, data:CountData):
         '''
         Sets the count data to the provided data object.
@@ -34,11 +42,20 @@ class SceneCountsItemMixin:
             return None
 
     def hoverEnterEvent(self, event:QtWidgets.QGraphicsSceneHoverEvent):
+        '''
+        Upon beginning to hover, the tool tip with animal counts will be
+        displayed and the drop shadow effect will be enabled.
+        '''
         if isinstance(self.countData(), CountData):
             tip = self.countData().toToolTip()
         else:
             tip = None
         self.setToolTip(tip)
 
+        self._shadowEffect.setEnabled(True)
+
     def hoverLeaveEvent(self, event:QtWidgets.QGraphicsSceneHoverEvent):
-        print('leave event -- got to hide that "..." widget now')
+        '''
+        When leaving the widget, the hover effects should be turned off.
+        '''
+        self._shadowEffect.setEnabled(False)
