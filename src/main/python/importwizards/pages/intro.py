ac53f9834b8151c0130803351a5198b09d886966
nprezant <34352369+nprezant@users.noreply.github.com>
2020-07-29 21:54:39 -0600

    Import wizard - add invalid path text when import path is invalid

--- src/main/python/importwizards/pages/intro.py
+++ src/main/python/importwizards/pages/intro.py
@@ -21,17 +21,23 @@ class IntroPage(QtWidgets.QWizardPage):
         self.pathEdit = QtWidgets.QLineEdit()
         self.pathEdit.setValidator(DirectoryValidator())
         self.pathEdit.textChanged.connect(lambda text: self.completeChanged.emit())
+        self.pathEdit.textChanged.connect(lambda text: self._updateInvalidPathVisibility())
         self.browse = QtWidgets.QPushButton('...')
+        self.browse.setToolTip('Browse')
         self.browse.setMaximumWidth(
             self.browse.fontMetrics().boundingRect('...').width() + 20
         )
         self.browse.clicked.connect(self._chooseImportFolder)
         self.registerField('importFolder', self.pathEdit)
 
+        self.invalidPathLabel = QtWidgets.QLabel('Please enter a valid folder path or choose one with the browse button')
+        self.invalidPathLabel.setStyleSheet('QLabel { color: red }')
+
         layout = QtWidgets.QGridLayout()
         layout.addWidget(self.pathLabel, 0, 0)
         layout.addWidget(self.pathEdit, 0, 1)
         layout.addWidget(self.browse, 0, 2)
+        layout.addWidget(self.invalidPathLabel, 1, 0, 1, 3)
         self.setLayout(layout)
 
     def initializePage(self):
@@ -57,6 +63,12 @@ class IntroPage(QtWidgets.QWizardPage):
 
         if not folder == '':
             self.pathEdit.setText(folder)
+
+    def _updateInvalidPathVisibility(self):
+        if self.pathEdit.hasAcceptableInput():
+            self.invalidPathLabel.hide()
+        else:
+            self.invalidPathLabel.show()
 
     def isComplete(self):
         return self.pathEdit.hasAcceptableInput()
