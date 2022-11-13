9e443088f88d9a886b6448b0b6b07c43afd1f80e
Noah Prezant <noah.prezant@protonmail.ch>
2022-11-12 17:46:21 -0700

    Add sanity checks for Qt6

---
+++ tools/hello_qt.py
@@ -0,0 +1,41 @@
+"""
+Useful for making sure PySide is working properly.
+Reference: https://doc.qt.io/qtforpython-6/quickstart.html
+Requires installing PySide6: `pip install PySide6`
+Run with python: `python tools/hello_qt.py`
+"""
+
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice([x for x in self.hello if x != self.text.text()]))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
